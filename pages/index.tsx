6a342137e29ef1914f185db7053a1062394b7a48
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-20 12:00:38 +0100

    create draft layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,17 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+
 import { firestore } from "../firebase/clientApp";
+import Layout from "../components/Layout";
+import Main from "../components/Main";
 
 const Home: NextPage = () => {
-
-    return <div>Hello</div>;
+    return (
+        <Layout>
+            <Main />
+        </Layout>
+    );
 };
 
 export default Home;
