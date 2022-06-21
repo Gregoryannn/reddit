4647e2ed80b0bc37c60310cd80ec80dc3448a4cf
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-21 09:45:47 +0100

    add auth modal skeleton and atoms

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../Navbar";
+import AuthModal from "./AuthModal";
 
 const Layout: React.FC = ({ children }) => {
     return (
         <>
             <Navbar />
+            <AuthModal />
         </>
     );
 };
-
 export default Layout;
