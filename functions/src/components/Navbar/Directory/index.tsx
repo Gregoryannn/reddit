27b99f4ee2fe0aac8ca35c06866093dbc84e0aa4
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-26 11:57:06 +0100

    move auth modal to navbar

--- functions/src/components/Navbar/Directory/index.tsx
+++ functions/src/components/Navbar/Directory/index.tsx
@@ -1,82 +1,33 @@
 import React from "react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
-import {
-    Box,
-    Flex,
-    Icon,
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuList,
-    Text,
-} from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "../../atoms/authModalAtom";
+import { auth } from "../../firebase/clientApp";
+import RightContentWrapper from "./RightContent";
+import SearchInput from "./SearchInput";
+import Directory from "./Directory";
+import AuthModal from "../Layout/Auth/AuthModal";
 
-import { CgProfile } from "react-icons/cg";
-import { MdOutlineLogin } from "react-icons/md";
-import { GrAdd } from "react-icons/gr";
-import { TiHome } from "react-icons/ti";
-import { logout } from "../../../firebase/authFunctions";
-
-type DirectoryProps = {};
-const Directory: React.FC<DirectoryProps> = () => {
+const Navbar: React.FC = () => {
+    const [user] = useAuthState(auth);
+    // const currentUser = useRecoilValue(userState); // will implement later with custom user object
     return (
-        <Menu>
-            <MenuButton
-                cursor="pointer"
-                padding="0px 6px"
-                borderRadius="4px"
-                _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
-                mr={2}
-                ml={2}
-            >
-                <Flex
-                    alignItems="center"
-                    justifyContent="space-between"
-                    width={{ base: "auto", lg: "200px" }}
-                >
-                    <Flex alignItems="center">
-                        <>
-                            <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
-                            <Box
-                                display={{ base: "none", lg: "flex" }}
-                                flexDirection="column"
-                                fontSize="10pt"
-                            >
-                                <Text fontWeight={600}>Home</Text>
-                            </Box>
-                        </>
-                    </Flex>
-                    <ChevronDownIcon color="gray.500" />
-                </Flex>
-            </MenuButton>
-            <MenuList width="100%">
-                <>
-                                     {/* <Flex direction="column" mt={2} mb={4}> */}
-                        <Text pl={3} fontSize="7pt" fontWeight={500} color="gray.500">
-                            MY COMMUNITIES
-                        </Text>
-                        <MenuItem width="100%" fontSize="10pt" _hover={{ bg: "gray.100" }}>
-                            <Flex alignItems="center">
-                            <Icon fontSize={20} mr={2} as={GrAdd} />
-                                Create Community
-                            </Flex>
-                        </MenuItem>
-                    
-                            {/* </Flex> */}
-                            <Text pl={3} fontSize="7pt" fontWeight={500} color="gray.500">
-                                FEEDS
-                            </Text>
-                            <MenuItem width="100%" fontSize="10pt" _hover={{ bg: "gray.100" }}>
-                                <Flex alignItems="center">
-                                 
-                                    <Icon fontSize={20} mr={2} as={GrAdd} />
-                                    Create Community
-                                </Flex>
-                            </MenuItem>
-                        </>
-                    </MenuList>
-                </Menu>
-                );
+        <Flex
+            bg="white"
+            height="44px"
+            padding="6px 12px"
+            justifyContent={{ md: "space-between" }}
+        >
+            <Box width={{ base: "40px", md: "auto" }} mr={2}>
+                <Image src="/images/redditlogo.png" height="30px" />
+            </Box>
+            {user && <Directory />}
+            <AuthModal />
+            <SearchInput user={user as User} />
+            <RightContentWrapper user={user as User} />
+        </Flex>
+    );
 };
-
-export default Directory;
+export default Navbar;
