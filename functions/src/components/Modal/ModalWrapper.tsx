e19c79d7d5cdbcf386c20ae7cd5e6a9bd25fedd8
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-27 12:50:27 +0100

    restructure modal files and logic

--- functions/src/components/Modal/ModalWrapper.tsx
+++ functions/src/components/Modal/ModalWrapper.tsx
@@ -11,32 +11,23 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 
-type ModalWrapperProps = {};
-
-const ModalWrapper: React.FC<ModalWrapperProps> = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    return (
-        <>
-            <Button onClick={onOpen}>Open Modal</Button>
-
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <span>Hello</span>
-                    </ModalBody>
-
-                    <ModalFooter>
-                        <Button colorScheme="blue" mr={3} onClick={onClose}>
-                            Close
-                        </Button>
-                        <Button variant="ghost">Secondary Action</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </>
-    );
+type ModalWrapperProps = {
+    isOpen: boolean;
+    onClose: () => void;
 };
-export default ModalWrapper;
+
+const ModalWrapper: React.FC<ModalWrapperProps> = ({
+        children,
+        isOpen,
+        onClose,
+    }) => {
+        return (
+            <>
+                <Modal isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                        <ModalContent>{children}</ModalContent>
+                </Modal>
+            </>
+        );
+    };
+    export default ModalWrapper;
--- functions/src/components/Navbar/RightContent/index.tsx
+++ functions/src/components/Navbar/RightContent/index.tsx
@@ -1,81 +1,27 @@
 import React from "react";
-import { AddIcon } from "@chakra-ui/icons";
-import { Box, Flex, Icon } from "@chakra-ui/react";
-import { BsArrowUpRightCircle, BsChatDots } from "react-icons/bs";
-import { GrAdd } from "react-icons/gr";
-import {
-    IoFilterCircleOutline,
-    IoNotificationsOutline,
-    IoVideocamOutline,
-} from "react-icons/io5";
-type ActionIconsProps = {};
-const ActionIcons: React.FC<ActionIconsProps> = () => {
-    return (
-        <Flex alignItems="center" flexGrow={1}>
-            <Box
-                display={{ base: "none", md: "flex" }}
-                alignItems="center"
-                borderRight="1px solid"
-                borderColor="gray.200"
-            >
-                <Flex
-                    mr={1.5}
-                    ml={1.5}
-                    padding={1}
-                    cursor="pointer"
-                    _hover={{ bg: "gray.200" }}
-                >
-                    <Icon as={BsArrowUpRightCircle} fontSize={20} />
-                </Flex>
-                <Flex
-                    mr={1.5}
-                    ml={1.5}
-                    padding={1}
-                    cursor="pointer"
-                    _hover={{ bg: "gray.200" }}
-                >
-                    <Icon as={IoFilterCircleOutline} fontSize={22} />
-                </Flex>
-                <Flex
-                    mr={1.5}
-                    ml={1.5}
-                    padding={1}
-                    cursor="pointer"
-                    _hover={{ bg: "gray.200" }}
-                >
-                    <Icon as={IoVideocamOutline} fontSize={22} />
-                </Flex>
-            </Box>
-            <>
-                <Flex
-                    mr={1.5}
-                    ml={1.5}
-                    padding={1}
-                    cursor="pointer"
-                    _hover={{ bg: "gray.200" }}
-                >
-                    <Icon as={BsChatDots} fontSize={20} />
-                </Flex>
-                <Flex
-                    mr={1.5}
-                    ml={1.5}
-                    padding={1}
-                    cursor="pointer"
-                    _hover={{ bg: "gray.200" }}
-                >
-                    <Icon as={IoNotificationsOutline} fontSize={20} />
-                </Flex>
-                <Flex
-                    mr={3}
-                    ml={1.5}
-                    padding={1}
-                    cursor="pointer"
-                    _hover={{ bg: "gray.200" }}
-                >
-                    <Icon as={GrAdd} fontSize={20} />
-                </Flex>
-            </>
-        </Flex>
-    );
+import { Flex } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import AuthModal from "../../Modal/Auth";
+import AuthButtons from "./AuthButtons";
+import Icons from "./Icons";
+import MenuWrapper from "./ProfileMenu/MenuWrapper";
+
+type RightContentProps = {
+    user: User;
 };
-export default ActionIcons;
+
+const RightContent: React.FC<RightContentProps> = ({ user }) => {
+        return (
+    <Flex
+      // width={{ sm: "auto", md: "300px" }}
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      {user ? <Icons /> : <AuthButtons />}
+      <MenuWrapper />
+ 
+    </>
+        );
+    };
+
+export default RightContent;
