f85a3f0322109d03efae079d09293864f877ddbd
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-28 10:14:09 +0100

    add community modal skeleton

--- functions/src/components/Modal/CreateCommunity/index.tsx
+++ functions/src/components/Modal/CreateCommunity/index.tsx
@@ -3,40 +3,43 @@ import {
     ModalCloseButton,
     ModalBody,
     Flex,
+    Text,
+    Divider,
+    Box,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import ModalWrapper from "../ModalWrapper";
-
 type CreateCommunityModalProps = {
     isOpen: boolean;
     handleClose: () => void;
 };
-
 const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
     isOpen,
     handleClose,
 }) => {
     return (
-        <ModalWrapper isOpen={isOpen} onClose={handleClose}>
-            <ModalHeader display="flex" flexDirection="column">
-                Create a community
-            </ModalHeader>
-            <ModalCloseButton />
-            <ModalBody
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center"
-                pb={6}
-            >
-                <Flex
-                    direction="column"
-                    alignItems="center"
-                    justifyContent="center"
-                    width="70%"
-                ></Flex>
-            </ModalBody>
-        </ModalWrapper>
-    );
+    <ModalWrapper isOpen={isOpen} onClose={handleClose}>
+      <ModalHeader
+        display="flex"
+        flexDirection="column"
+        fontSize={15}
+        padding={3}
+      >
+        Create a community
+      </ModalHeader>
+      <Box pr={3} pl={3}>
+        <Divider />
+        <ModalCloseButton />
+        <ModalBody display="flex" flexDirection="column" padding="10px 0px">
+          <Text fontWeight={600} fontSize={15}>
+            Name
+          </Text>
+          <Text fontSize={11} color="gray.500">
+            Community names including capitalization cannot be changed
+          </Text>
+        </ModalBody>
+      </Box>
+    </ModalWrapper >
+  );
 };
 export default CreateCommunityModal;
--- functions/src/components/Navbar/Directory/index.tsx
+++ functions/src/components/Navbar/Directory/index.tsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
     Box,
+    Divider,
     Flex,
     Icon,
     Menu,
     MenuButton,
+    MenuDivider,
     MenuItem,
     MenuList,
     Text,
@@ -16,6 +18,7 @@ import { GrAdd } from "react-icons/gr";
 import { TiHome } from "react-icons/ti";
 import { logout } from "../../../firebase/authFunctions";
 import CreateCommunityModal from "../../Modal/CreateCommunity";
+import Communities from "./Communities";
 
 type DirectoryProps = {};
 
@@ -23,78 +26,42 @@ const Directory: React.FC<DirectoryProps> = () => {
     const [open, setOpen] = useState(false);
     const handleClose = () => setOpen(false);
     return (
-   <>
-      <CreateCommunityModal isOpen={open} handleClose={handleClose} />
-      <Menu>
-        <MenuButton
-          cursor="pointer"
-          padding="0px 6px"
-          borderRadius="4px"
-          _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
-          mr={2}
-          ml={2}
-        >
-            <Flex alignItems="center">
-              <Icon fontSize={20} mr={2} as={GrAdd} />
-              Create Community
-            </Flex>
-          </MenuItem>
-          {/* </Flex> */}
-          <Text pl={3} fontSize="7pt" fontWeight={500} color="gray.500">
-            FEEDS
-          </Text>
-          <MenuItem width="100%" fontSize="10pt" _hover={{ bg: "gray.100" }}>
-          <Flex
-            alignItems="center"
-            justifyContent="space-between"
-            width={{ base: "auto", lg: "200px" }}
-          >
-            <Flex alignItems="center">
-              <>
-                <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
-                <Box
-                  display={{ base: "none", lg: "flex" }}
-                  flexDirection="column"
-                  fontSize="10pt"
+        <>
+            <CreateCommunityModal isOpen={open} handleClose={handleClose} />
+            <Menu>
+                <MenuButton
+                    cursor="pointer"
+                    padding="0px 6px"
+                    borderRadius="4px"
+                    _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+                    mr={2}
+                    ml={2}
                 >
-                  <Text fontWeight={600}>Home</Text>
-                </Box>
-              </>
-            </Flex>
-              <ChevronDownIcon color="gray.500" />
-          </Flex>
-        </MenuButton>
-        <MenuList width="100%">
-          <>
-            {/* <Flex direction="column" mt={2} mb={4}> */}
-            <Text pl={3} fontSize="7pt" fontWeight={500} color="gray.500">
-              MY COMMUNITIES
-            </Text>
-            <MenuItem
-              width="100%"
-              fontSize="10pt"
-              _hover={{ bg: "gray.100" }}
-              onClick={() => setOpen(true)}
-            >
-              <Flex alignItems="center">
-                <Icon fontSize={20} mr={2} as={GrAdd} />
-                Create Community
-              </Flex>
-            </MenuItem>
-            {/* </Flex> */}
-            <Text pl={3} fontSize="7pt" fontWeight={500} color="gray.500">
-              FEEDS
-            </Text>
-            <MenuItem width="100%" fontSize="10pt" _hover={{ bg: "gray.100" }}>
-              <Flex alignItems="center">
-                <Icon fontSize={20} mr={2} as={GrAdd} />
-                Create Community
-              </Flex>
-            </MenuItem>
-          </>
-        </MenuList>
-      </Menu >
-    </>
-  );
+                    <Flex
+                        alignItems="center"
+                        justifyContent="space-between"
+                        width={{ base: "auto", lg: "200px" }}
+                    >
+                        <Flex alignItems="center">
+                            <>
+                                <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
+                                <Box
+                                    display={{ base: "none", lg: "flex" }}
+                                    flexDirection="column"
+                                    fontSize="10pt"
+                                >
+                                    <Text fontWeight={600}>Home</Text>
+                                </Box>
+                            </>
+                        </Flex>
+                        <ChevronDownIcon color="gray.500" />
+                    </Flex>
+                </MenuButton>
+                <MenuList width="100%">
+                    <Communities />
+                </MenuList>
+            </Menu>
+        </>
+    );
 };
 export default Directory;
