e5629ac0d303ad97badc4a5e59cd8cfbe1c8558e
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-07-03 11:47:54 +0100

    fix wrapper padding

--- functions/src/components/Community/ContentWrapper.tsx
+++ functions/src/components/Community/ContentWrapper.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { Box, Flex } from "@chakra-ui/react";
 import CreatePostLink from "./CreatePostLink";
-
 type ContentWrapperProps = {};
 
 const ContentWrapper: React.FC<ContentWrapperProps> = () => {
     return (
-        <Flex justify="center" border="1px solid pink" p="16px">
-            <Flex width="95%" maxWidth="860px" border="1px solid red">
-                {/* <Box width="65%" border="1px solid green"></Box> */}
-                {/* Left Content */}
-                <CreatePostLink />
-                <Box flexGrow={1} border="1px solid blue">
-                    hello
-                </Box>
+            <Flex justify="center" border="1px solid pink" p="16px 0px">
+                <Flex width="95%" maxWidth="860px" border="1px solid red">
+                    {/* <Box width="65%" border="1px solid green"></Box> */}
+                    {/* Left Content */}
+                    <CreatePostLink />
+                        <Box
+                            display={{ base: "none", md: "flex" }}
+                            flexGrow={1}
+                            border="1px solid blue"
+                        >
+                            hello
+                        </Box>
 
-                {/* Right Content */}
+                        {/* Right Content */}
+                </Flex>
             </Flex>
-        </Flex>
-    );
+            );
 };
-export default ContentWrapper;
+            export default ContentWrapper;
