4e53db01a12db0042eb6b979bb4eb37f73223547
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-07-11 10:31:20 +0100

    add fetch and create comment loading states

--- functions/src/components/Post/Comments/index.tsx
+++ functions/src/components/Post/Comments/index.tsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Box, Flex, Stack } from "@chakra-ui/react";
+
+import {
+    Box,
+    Flex,
+    Skeleton,
+    SkeletonCircle,
+    SkeletonText,
+    Stack,
+} from "@chakra-ui/react";
 
 import {
     collection,
@@ -21,105 +29,104 @@ import { auth, firestore } from "../../../firebase/clientApp";
 import CommentItem, { Comment } from "./CommentItem";
 import CommentInput from "./Input";
 
-
 type CommentsProps = {
     selectedPost: Post;
     community: string;
 };
 
 const Comments: React.FC<CommentsProps> = ({ selectedPost, community }) => {
-        const [user] = useAuthState(auth);
-        const [comment, setComment] = useState("");
-        const [comments, setComments] = useState<Comment[]>([]);
-        const [loading, setLoading] = useState(false);
-        const setAuthModalState = useSetRecoilState(authModalState);
-        const setPostItemState = useSetRecoilState(postState);
+    const [user] = useAuthState(auth);
+    const [comment, setComment] = useState("");
+    const [comments, setComments] = useState<Comment[]>([]);
+    const [commentFetchLoading, setCommentFetchLoading] = useState(false);
+    const [commentCreateLoading, setCommentCreateLoading] = useState(false);
+    const setAuthModalState = useSetRecoilState(authModalState);
+    const setPostItemState = useSetRecoilState(postState);
+    const onCreateComment = async (comment: string) => {
 
-        const onCreateComment = async (comment: string) => {
-            if (!user) {
-                setAuthModalState({ open: true, view: "login" });
-                return;
-            }
-            setLoading(true);
-            try {
-                const batch = writeBatch(firestore);
-                // Create comment document
-                const commentDocRef = doc(collection(firestore, "comments"));
-                batch.set(commentDocRef, {
-                    postId: selectedPost.id,
+        if (!user) {
+            setAuthModalState({ open: true, view: "login" });
+            return;
+        }
+
+        setCommentCreateLoading(true);
+        try {
+            const batch = writeBatch(firestore);
+
+            // Create comment document
+            const commentDocRef = doc(collection(firestore, "comments"));
+            batch.set(commentDocRef, {
+                postId: selectedPost.id,
+                authorId: user.uid,
+                communityId: community,
+                text: comment,
+                postTitle: selectedPost.title,
+                createdAt: serverTimestamp(),
+            });
+
+            // Update post numberOfComments
+            batch.update(doc(firestore, "posts", selectedPost.id), {
+                numberOfComments: increment(1),
+            });
+            await batch.commit();
+            setComment("");
+            const { id: postId, title } = selectedPost;
+            setComments((prev) => [
+                {
+                    id: commentDocRef.id,
                     authorId: user.uid,
                     communityId: community,
+                    postId,
+                    postTitle: title,
                     text: comment,
-                    postTitle: selectedPost.title,
-                    createdAt: serverTimestamp(),
-                });
+                    createdAt: {
+                        seconds: Date.now() / 1000,
+                    },
+                } as Comment,
+                ...prev,
+            ]);
 
-                // Update post numberOfComments
-                batch.update(doc(firestore, "posts", selectedPost.id), {
-                        numberOfComments: increment(1),
-                    });
-                    await batch.commit();
-                    setComment("");
+            // Fetch posts again to update number of comments
+            setPostItemState((prev) => ({
+                ...prev,
+                selectedPost: {
+                    ...prev.selectedPost,
+                    numberOfComments: prev.selectedPost?.numberOfComments! + 1,
+                } as Post,
+                postUpdateRequired: true,
+            }));
+        } catch (error: any) {
+            console.log("onCreateComment error", error.message);
+        }
+        setCommentCreateLoading(false);
+    };
 
-                const { id: postId, title } = selectedPost;
-                    setComments((prev) => [
-                        {
-                            id: commentDocRef.id,
-                            authorId: user.uid,
-                            communityId: community,
-                            postId,
-                            postTitle: title,
-                            text: comment,
-                            createdAt: {
-                                seconds: Date.now() / 1000,
-                            },
-                        } as Comment,
-                        ...prev,
-                    ]);
+    const getPostComments = async () => {
+        setCommentFetchLoading(true);
+        try {
+            const commentsQuery = query(
+                collection(firestore, "comments"),
+                where("postId", "==", selectedPost.id),
+                orderBy("createdAt", "desc")
+            );
+            const commentDocs = await getDocs(commentsQuery);
+            const comments = commentDocs.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setComments(comments as Comment[]);
+        } catch (error: any) {
+            console.log("getPostComments error", error.message);
+        }
+        setCommentFetchLoading(false);
+    };
 
-// Fetch posts again to update number of comments
-setPostItemState((prev) => ({
-    ...prev,
-    selectedPost: {
-        ...prev.selectedPost,
-        numberOfComments: prev.selectedPost?.numberOfComments! + 1,
-    } as Post,
-    postUpdateRequired: true,
-}));
-    } catch (error: any) {
-    console.log("onCreateComment error", error.message);
-}
-setLoading(false);
-  };
-
-const getPostComments = async () => {
-    setLoading(true);
-    try {
-        const commentsQuery = query(
-            collection(firestore, "comments"),
-            where("postId", "==", selectedPost.id),
-            orderBy("createdAt", "desc")
-        );
-        const commentDocs = await getDocs(commentsQuery);
-        const comments = commentDocs.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-        }));
-        setComments(comments as Comment[]);
-    } catch (error: any) {
-        console.log("getPostComments error", error.message);
-    }
-    setLoading(false);
-};
-
-useEffect(() => {
-    // get comments
-    getPostComments();
-}, []);
-
-
-return (
-    <Box bg="white" p={2} borderRadius="0px 0px 4px 4px">
+    useEffect(() => {
+        // get comments
+        getPostComments();
+    }, []);
+    return (
+        <Box bg="white" p={2} borderRadius="0px 0px 4px 4px">
             <Flex
                 direction="column"
                 pl={10}
@@ -131,18 +138,31 @@ return (
                 <CommentInput
                     comment={comment}
                     setComment={setComment}
-                    loading={loading}
+                    loading={commentCreateLoading}
                     user={user}
                     onCreateComment={onCreateComment}
                 />
-          
             </Flex>
             <Stack spacing={6} p={2}>
-                {comments.map((item: Comment) => (
-                    <CommentItem key={item.id} comment={item} />
-                ))}
+             
+                {commentFetchLoading ? (
+                    <>
+                        {[0, 1, 2].map((item) => (
+                            <Box padding="6" bg="white">
+                                <SkeletonCircle size="10" />
+                                <SkeletonText mt="4" noOfLines={2} spacing="4" />
+                            </Box>
+                        ))}
+                    </>
+                ) : (
+                    <>
+                        {comments.map((item: Comment) => (
+                            <CommentItem key={item.id} comment={item} />
+                        ))}
+                    </>
+                )}
             </Stack>
-    </Box>
-);
+        </Box>
+    );
 };
 export default Comments;
