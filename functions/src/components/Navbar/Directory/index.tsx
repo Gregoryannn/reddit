34c65f05cba5fca34c2c0346e7f0f2aa309a3c42
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-07-14 14:57:22 +0100

    add directory menu select logic

--- functions/src/components/Navbar/Directory/index.tsx
+++ functions/src/components/Navbar/Directory/index.tsx
@@ -8,53 +8,77 @@ import {
     MenuList,
     Text,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { IconType } from "react-icons";
 import { TiHome } from "react-icons/ti";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+
+import {
+    DirectoryMenuItem,
+    directoryMenuState,
+} from "../../../atoms/directoryMenuAtom";
+
 import CreateCommunityModal from "../../Modal/CreateCommunity";
 import Communities from "./Communities";
-type DirectoryProps = {};
-const Directory: React.FC<DirectoryProps> = () => {
-    const [open, setOpen] = useState(false);
-    const handleClose = () => setOpen(false);
-    return (
-        <Menu>
-            {({ isOpen }) => (
-                <>
-                    <MenuButton
-                        cursor="pointer"
-                        padding="0px 6px"
-                        borderRadius="4px"
-                        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
-                        mr={2}
-                        ml={2}
-                    >
-                        <Flex
-                            alignItems="center"
-                            justifyContent="space-between"
-                            width={{ base: "auto", lg: "200px" }}
+
+
+const Directory: React.FC = () => {
+        const router = useRouter();
+        const [open, setOpen] = useState(false);
+        const handleClose = () => setOpen(false);
+
+        // Use <Link> for initial build; implement directory logic near end
+        const directoryState = useRecoilValue(directoryMenuState);
+
+        useEffect(() => {
+            router.push(directoryState.link);
+        }, [directoryState]);
+
+        return (
+            <Menu>
+                {({ isOpen }) => (
+                    <>
+                        <MenuButton
+                            cursor="pointer"
+                            padding="0px 6px"
+                            borderRadius="4px"
+                            _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+                            mr={2}
+                            ml={2}
                         >
-                            <Flex alignItems="center">
-                                <>
-                                    <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
-                                    <Box
-                                        display={{ base: "none", lg: "flex" }}
-                                        flexDirection="column"
-                                        fontSize="10pt"
-                                    >
-                                        <Text fontWeight={600}>Home</Text>
-                                    </Box>
-                                </>
+                            <Flex
+                                alignItems="center"
+                                justifyContent="space-between"
+                                width={{ base: "auto", lg: "200px" }}
+                            >
+                                <Flex alignItems="center">
+                                    <>
+                                        <Icon
+                                            fontSize={24}
+                                            mr={{ base: 1, md: 2 }}
+                                            color={directoryState.iconColor}
+                                            as={directoryState.icon}
+                                        />
+                                        <Box
+                                            display={{ base: "none", lg: "flex" }}
+                                            flexDirection="column"
+                                            fontSize="10pt"
+                                        >
+                                            <Text fontWeight={600}>{directoryState.displayText}</Text>
+                                        </Box>
+                                    </>
+                                </Flex>
+                                <ChevronDownIcon color="gray.500" />
                             </Flex>
-                            <ChevronDownIcon color="gray.500" />
-                        </Flex>
-                    </MenuButton>
-                     <MenuList maxHeight="300px" overflow="scroll" overflowX="hidden">
+                        </MenuButton>
+                        <MenuList maxHeight="300px" overflow="scroll" overflowX="hidden">
                             <Communities menuOpen={isOpen} />
                         </MenuList>
                     </>
-      )}
-                </Menu>
-            );
-};
-
-export default Directory;
+                )}
+            </Menu>
+        );
+    };
+    export default Directory;
