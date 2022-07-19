57c6273c54705fa8da17095612470e4ec4f5f481
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-07-19 10:25:58 +0100

    add responsive logo

--- functions/src/components/Navbar/index.tsx
+++ functions/src/components/Navbar/index.tsx
@@ -13,13 +13,10 @@ import RightContent from "./RightContent";
 import SearchInput from "./SearchInput";
 import router from "next/router";
 import useDirectory from "../../hooks/useDirectory";
-
 const Navbar: React.FC = () => {
     const [user] = useAuthState(auth);
-
     // Use <Link> for initial build; implement directory logic near end
     const { onSelectMenuItem } = useDirectory();
-
     return (
         <Flex
             bg="white"
@@ -27,18 +24,24 @@ const Navbar: React.FC = () => {
             padding="6px 12px"
             justifyContent={{ md: "space-between" }}
         >
-            <Box
+                <Flex
+        align="center"
                 width={{ base: "40px", md: "auto" }}
-                mr={2}
+                mr={{ base: 0, md: 2 }}
                 cursor="pointer"
                 onClick={() => onSelectMenuItem(defaultMenuItem)}
             >
-                <Image src="/images/redditlogo.png" height="30px" />
-            </Box>
-            {user && <Directory />}
-            <SearchInput user={user as User} />
-            <RightContent user={user as User} />
+            <Image src="/images/redditFace.svg" height="30px" />
+            <Image
+                display={{ base: "none", md: "unset" }}
+                src="/images/redditText.svg"
+                height="46px"
+            />
         </Flex>
-    );
+      { user && <Directory /> }
+      <SearchInput user={user as User} />
+      <RightContent user={user as User} />
+    </Flex >
+  );
 };
 export default Navbar;
--- functions/src/pages/r/[community]/index.tsx
+++ functions/src/pages/r/[community]/index.tsx
@@ -1,188 +1,65 @@
-import { useEffect, useState } from "react";
-import { Box, Stack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
-    collection,
-    DocumentData,
-    getDocs,
-    limit,
-    onSnapshot,
-    orderBy,
-    query,
-    QuerySnapshot,
-    where,
-} from "firebase/firestore";
-import type { NextPage } from "next";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { Post, PostVote } from "../atoms/postsAtom";
-import CreatePostLink from "../components/Community/CreatePostLink";
-import PageContentLayout from "../components/Layout/PageContent";
-import PostLoader from "../components/Post/Loader";
-import PostItem from "../components/Post/PostItem";
-import { auth, firestore } from "../firebase/clientApp";
-import usePosts from "../hooks/usePosts";
-import useCommunityData from "../hooks/useCommunityData";
-import Recommendations from "../components/Community/Recommendations";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { communityState } from "../atoms/communitiesAtom";
-const Home: NextPage = () => {
-    const [user, loadingUser] = useAuthState(auth);
-    const {
-        postStateValue,
-        setPostStateValue,
-        onVote,
-        onSelectPost,
-        onDeletePost,
-        loading,
-        setLoading,
-    } = usePosts();
-    const mySnippets = useRecoilValue(communityState).mySnippets;
-    // WILL NEED TO HANDLE CASE OF NO USER
-    const getUserHomePosts = async () => {
-        console.log("GETTING USER FEED");
+    Box,
+    Flex,
+    Icon,
+    Menu,
+    MenuButton,
+    MenuList,
+    Text,
+} from "@chakra-ui/react";
 
-        setLoading(true);
-        try {
-            /**
-             * if snippets has no length (i.e. user not in any communities yet)
-             * do query for 20 posts ordered by voteStatus
-             */
-            const myCommunityIds = mySnippets.map((snippet) => snippet.communityId);
-            let postPromises: Array<Promise<QuerySnapshot<DocumentData>>> = [];
-            [0, 1, 2].forEach((index) => {
-                if (!myCommunityIds[index]) return;
-                postPromises.push(
-                    getDocs(
-                        query(
-                            collection(firestore, "posts"),
-                            where("communityId", "==", myCommunityIds[index]),
-                            limit(2)
-                        )
-                    )
-                );
-            });
-            const queryResults = await Promise.all(postPromises);
-            const feedPosts: Post[] = [];
-            queryResults.forEach((result) => {
-                const posts = result.docs.map((doc) => ({
-                    id: doc.id,
-                    ...doc.data(),
-                })) as Post[];
-                feedPosts.push(...posts);
-            });
-            console.log("HERE ARE FEED POSTS", feedPosts);
-            setPostStateValue((prev) => ({
-                ...prev,
-                posts: feedPosts,
-            }));
-            // if not in any, get 5 communities ordered by number of members
-            // for each one, get 2 posts ordered by voteStatus and set these to postState posts
-        } catch (error: any) {
-            console.log("getUserHomePosts error", error.message);
-        }
-        setLoading(false);
-    };
+import useDirectory from "../../../hooks/useDirectory";
+import Communities from "./Communities";
 
-    const getNoUserHomePosts = async () => {
-        console.log("GETTING NO USER FEED");
-        setLoading(true);
-        try {
-            const postQuery = query(
-                collection(firestore, "posts"),
-                orderBy("voteStatus", "desc"),
-                limit(20)
-            );
-            const postDocs = await getDocs(postQuery);
-            const posts = postDocs.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            }));
-            console.log("NO USER FEED", posts);
-
-            setPostStateValue((prev) => ({
-                ...prev,
-                posts: posts as Post[],
-            }));
-        } catch (error: any) {
-            console.log("getNoUserHomePosts error", error.message);
-        }
-        setLoading(false);
-    };
-
-    const getUserPostVotes = async () => {
-        const postIds = postStateValue.posts.map((post) => post.id);
-        const postVotesQuery = query(
-            collection(firestore, `users/${user?.uid}/postVotes`),
-            where("postId", "in", postIds)
-        );
-        const unsubscribe = onSnapshot(postVotesQuery, (querySnapshot) => {
-            const postVotes = querySnapshot.docs.map((postVote) => ({
-                id: postVote.id,
-                ...postVote.data(),
-            }));
-            setPostStateValue((prev) => ({
-                ...prev,
-                postVotes: postVotes as PostVote[],
-            }));
-        });
-        return () => unsubscribe();
-    };
-
-    if (!mySnippets.length) return;
-
-    if (user) {
-        getUserHomePosts();
-    }
-}, [user, mySnippets]);
-
-useEffect(() => {
-    if (!user && !loadingUser) {
-        getNoUserHomePosts();
-    }
-}, [user, loadingUser]);
-
-useEffect(() => {
-    if (!user?.uid || !postStateValue.posts.length) return;
-    getUserPostVotes();
-    // Clear postVotes on dismount
-    return () => {
-        setPostStateValue((prev) => ({
-            ...prev,
-            postVotes: [],
-        }));
-    };
-}, [postStateValue.posts, user?.uid]);
-return (
-    <PageContentLayout>
-        <>
-            <CreatePostLink />
-            {loading ? (
-                <PostLoader />
-            ) : (
-                <Stack>
-                    {postStateValue.posts.map((post: Post, index) => (
-                        <PostItem
-                            key={post.id}
-                            post={post}
-                            postIdx={index}
-                            onVote={onVote}
-                            onDeletePost={onDeletePost}
-                            userVoteValue={
-                                postStateValue.postVotes.find(
-                                    (item) => item.postId === post.id
-                                )?.voteValue
-                            }
-                            userIsCreator={user?.uid === post.creatorId}
-                            onSelectPost={onSelectPost}
-                            homePage
-                        />
-                    ))}
-                </Stack>
+const Directory: React.FC = () => {
+    const [open, setOpen] = useState(false);
+    const handleClose = () => setOpen(false);
+    const { directoryState } = useDirectory();
+    return (
+        <Menu>
+            {({ isOpen }) => (
+                <>
+                    <MenuButton
+                        cursor="pointer"
+                        padding="0px 6px"
+                        borderRadius="4px"
+                        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+                        mr={2}
+                        ml={{ base: 0, md: 2 }}
+                    >
+                        <Flex
+                            alignItems="center"
+                            justifyContent="space-between"
+                            width={{ base: "auto", lg: "200px" }}
+                        >
+                            <Flex alignItems="center">
+                                <>
+                                    <Icon
+                                        fontSize={24}
+                                        mr={{ base: 1, md: 2 }}
+                                        color={directoryState.iconColor}
+                                        as={directoryState.icon}
+                                    />
+                                    <Box
+                                        display={{ base: "none", lg: "flex" }}
+                                        flexDirection="column"
+                                        fontSize="10pt"
+                                    >
+                                        <Text fontWeight={600}>{directoryState.displayText}</Text>
+                                    </Box>
+                                </>
+                            </Flex>
+                            <ChevronDownIcon color="gray.500" />
+                        </Flex>
+                    </MenuButton>
+                    <MenuList maxHeight="300px" overflow="scroll" overflowX="hidden">
+                        <Communities menuOpen={isOpen} />
+                    </MenuList>
+                </>
             )}
-        </>
-        <>
-            <Recommendations />
-        </>
-    </PageContentLayout>
-);
+        </Menu>
+    );
 };
-export default Home;
+export default Directory;
