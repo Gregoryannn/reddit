ef75f90cbf17050e92490da7be942780bc66b641
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-07-20 11:55:29 +0100

    add isOpen to directory menu state

--- src/components/Navbar/Directory/index.tsx
+++ src/components/Navbar/Directory/index.tsx
@@ -1,3 +1,4 @@
+import import React, { useState } from "react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
     Box,
@@ -15,51 +16,55 @@ import Communities from "./Communities";
 const Directory: React.FC = () => {
     const [open, setOpen] = useState(false);
     const handleClose = () => setOpen(false);
-    const { directoryState } = useDirectory();
+
+    const { directoryState, toggleMenuOpen } = useDirectory();
 
     return (
-        <Menu>
-            {({ isOpen }) => (
-                <>
-                    <MenuButton
-                        cursor="pointer"
-                        padding="0px 6px"
-                        borderRadius="4px"
-                        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
-                        mr={2}
-                        ml={2}
-                    >
-                        <Flex
-                            alignItems="center"
-                            justifyContent="space-between"
-                            width={{ base: "auto", lg: "200px" }}
+            <Menu isOpen={directoryState.isOpen}>
+                {({ isOpen }) => (
+                    <>
+                        <MenuButton
+                            cursor="pointer"
+                            padding="0px 6px"
+                            borderRadius="4px"
+                            _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+                            mr={2}
+                            ml={{ base: 0, md: 2 }}
+                            onClick={toggleMenuOpen}
                         >
-                            <Flex alignItems="center">
-                                <>
-                                    <Icon
-                                        fontSize={24}
-                                        mr={{ base: 1, md: 2 }}
-                                        color={directoryState.iconColor}
-                                        as={directoryState.icon}
-                                    />
-                                    <Box
-                                        display={{ base: "none", lg: "flex" }}
-                                        flexDirection="column"
-                                        fontSize="10pt"
-                                    >
-                                        <Text fontWeight={600}>{directoryState.displayText}</Text>
-                                    </Box>
-                                </>
+                            <Flex
+                                alignItems="center"
+                                justifyContent="space-between"
+                                width={{ base: "auto", lg: "200px" }}
+                            >
+                                <Flex alignItems="center">
+                                    <>
+                                        <Icon
+                                            fontSize={24}
+                                            mr={{ base: 1, md: 2 }}
+                                            color={directoryState.selectedMenuItem.iconColor}
+                                            as={directoryState.selectedMenuItem.icon}
+                                        />
+                                        <Box
+                                            display={{ base: "none", lg: "flex" }}
+                                            flexDirection="column"
+                                            fontSize="10pt"
+                                        >
+                                            <Text fontWeight={600}>
+                                                {directoryState.selectedMenuItem.displayText}
+                                            </Text>
+                                        </Box>
+                                    </>
+                                </Flex>
+                                <ChevronDownIcon color="gray.500" />
                             </Flex>
-                            <ChevronDownIcon color="gray.500" />
-                        </Flex>
-                    </MenuButton>
-                    <MenuList maxHeight="300px" overflow="scroll" overflowX="hidden">
-                        <Communities menuOpen={isOpen} />
-                    </MenuList>
-                </>
-            )}
-        </Menu>
-    );
+                        </MenuButton>
+                        <MenuList maxHeight="300px" overflow="scroll" overflowX="hidden">
+                            <Communities menuOpen={isOpen} />
+                        </MenuList>
+                    </>
+                )}
+            </Menu>
+            );
 };
-export default Directory;
+            export default Directory;
--- src/hooks/useDirectory.ts
+++ src/hooks/useDirectory.ts
@@ -2,7 +2,6 @@ import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { communityState } from "../atoms/communitiesAtom";
-
 import {
     defaultMenuItem,
     DirectoryMenuItem,
@@ -12,31 +11,50 @@ import {
 import { FaReddit } from "react-icons/fa";
 
 const useDirectory = () => {
-    const [directoryState, setDirectoryState] =
-        useRecoilState(directoryMenuState);
-    const router = useRouter();
-    const communityStateValue = useRecoilValue(communityState);
-    const onSelectMenuItem = (menuItem: DirectoryMenuItem) => {
-        // setState here
-        setDirectoryState(menuItem);
+const [directoryState, setDirectoryState] =
+    useRecoilState(directoryMenuState);
+const router = useRouter();
+const communityStateValue = useRecoilValue(communityState);
+const onSelectMenuItem = (menuItem: DirectoryMenuItem) => {
+
+      // setState here
+        setDirectoryState((prev) => ({
+            ...prev,
+            selectedMenuItem: menuItem,
+        }));
         router.push(menuItem.link);
     };
+
+    const toggleMenuOpen = () => {
+        setDirectoryState((prev) => ({
+            ...prev,
+            isOpen: !directoryState.isOpen,
+        }));
+    };
+
     useEffect(() => {
         const { community } = router.query;
+
         const existingCommunity =
             communityStateValue.visitedCommunities[community as string];
+
         if (existingCommunity) {
-            setDirectoryState({
-                displayText: `r/${existingCommunity.id}`,
-                link: `r/${existingCommunity.id}`,
-                icon: FaReddit,
-                iconColor: "blue.500",
-            });
+           
+            setDirectoryState((prev) => ({
+                ...prev,
+                selectedMenuItem: {
+                    displayText: `r/${existingCommunity.id}`,
+                    link: `r/${existingCommunity.id}`,
+                    icon: FaReddit,
+                    iconColor: "blue.500",
+                },
+            }));
             return;
         }
         setDirectoryState(defaultMenuItem);
     }, [router.query?.community, communityStateValue.visitedCommunities]);
 
-    return { directoryState, onSelectMenuItem };
+    return { directoryState, onSelectMenuItem, toggleMenuOpen };
 };
+
 export default useDirectory;
