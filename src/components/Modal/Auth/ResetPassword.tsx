3ec3e3e69690464dbd1f1e2293b181915ced404d
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-07-24 09:17:48 +0100

    add toggleView to prop type

--- src/components/Modal/Auth/ResetPassword.tsx
+++ src/components/Modal/Auth/ResetPassword.tsx
@@ -2,37 +2,39 @@ import React, { useState } from "react";
 import { Button, Flex, Icon, Input, Text } from "@chakra-ui/react";
 import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
 import { BsDot, BsReddit } from "react-icons/bs";
-import { authModalState } from "../../../atoms/authModalAtom";
+import { authModalState, ModalView } from "../../../atoms/authModalAtom";
 import { auth } from "../../../firebase/clientApp";
 import { useSetRecoilState } from "recoil";
 
-const ResetPassword: React.FC = () => {
-    const setAuthModalState = useSetRecoilState(authModalState);
-    const [email, setEmail] = useState("");
-    const [success, setSuccess] = useState(false);
-    const [sendPasswordResetEmail, sending, error] =
-        useSendPasswordResetEmail(auth);
-
-    const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        await sendPasswordResetEmail(email);
-        setSuccess(true);
+type ResetPasswordProps = {
+        toggleView: (view: ModalView) => void;
     };
 
-    return (
-        <Flex direction="column" alignItems="center" width="100%">
-            <Icon as={BsReddit} color="brand.100" fontSize={40} mb={2} />
-            <Text fontWeight={700} mb={2}>
-                Reset your password
-            </Text>
-            {success ? (
-                <Text mb={4}>Check your email :)</Text>
-            ) : (
-        <>
-                    <Text fontSize="sm" textAlign="center" mb={2}>
-                        Enter the email associated with your account and we'll send you a
-                        reset link
-                    </Text>
+    const ResetPassword: React.FC<ResetPasswordProps> = ({ toggleView }) => {
+        const setAuthModalState = useSetRecoilState(authModalState);
+        const [email, setEmail] = useState("");
+        const [success, setSuccess] = useState(false);
+        const [sendPasswordResetEmail, sending, error] =
+            useSendPasswordResetEmail(auth);
+        const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            await sendPasswordResetEmail(email);
+            setSuccess(true);
+        };
+        return (
+            <Flex direction="column" alignItems="center" width="100%">
+                <Icon as={BsReddit} color="brand.100" fontSize={40} mb={2} />
+                <Text fontWeight={700} mb={2}>
+                    Reset your password
+                </Text>
+                {success ? (
+                    <Text mb={4}>Check your email :)</Text>
+                ) : (
+                    <>
+                        <Text fontSize="sm" textAlign="center" mb={2}>
+                            Enter the email associated with your account and we'll send you a
+                            reset link
+                        </Text>
                         <form onSubmit={onSubmit} style={{ width: "100%" }}>
                             <Input
                                 required
@@ -57,53 +59,52 @@ const ResetPassword: React.FC = () => {
                                 bg="gray.50"
                             />
                             <Text textAlign="center" fontSize="10pt" color="red">
-                            {error?.message}
-                        </Text>
-                        <Button
-                            width="100%"
-                            height="36px"
-                            mb={2}
-                            mt={2}
-                            type="submit"
-                            isLoading={sending}
-                        >
-                            Reset Password
-                        </Button>
-                    </form>
-                </>
-            )}
-            <Flex
-                alignItems="center"
-                fontSize="9pt"
-                color="blue.500"
-                fontWeight={700}
-                cursor="pointer"
-            >
-
-                <Text
-                    onClick={() =>
-                        setAuthModalState((prev) => ({
-                            ...prev,
-                            view: "login",
-                        }))
-                    }
+                                {error?.message}
+                            </Text>
+                            <Button
+                                width="100%"
+                                height="36px"
+                                mb={2}
+                                mt={2}
+                                type="submit"
+                                isLoading={sending}
+                            >
+                                Reset Password
+                            </Button>
+                        </form>
+                    </>
+                )}
+                <Flex
+                    alignItems="center"
+                    fontSize="9pt"
+                    color="blue.500"
+                    fontWeight={700}
+                    cursor="pointer"
                 >
-                    LOGIN
-                </Text>
-                <Icon as={BsDot} />
-
-                <Text
-                    onClick={() =>
-                        setAuthModalState((prev) => ({
-                            ...prev,
-                            view: "signup",
-                        }))
-                    }
-                >
-                    SIGN UP
-                </Text>
+                    <Text
+                        onClick={() =>
+                            setAuthModalState((prev) => ({
+                                ...prev,
+                                view: "login",
+                            }))
+                        }
+                    >
+                        LOGIN
+                    </Text>
+                    <Icon as={BsDot} />
+                    <Text
+                        onClick={() =>
+                            setAuthModalState((prev) => ({
+                                ...prev,
+                                view: "signup",
+                            }))
+                        }
+                    >
+                        SIGN UP
+                    </Text>
+                </Flex>
             </Flex>
-        </Flex>
-    );
-};
+        );
+    };
+
 export default ResetPassword;
